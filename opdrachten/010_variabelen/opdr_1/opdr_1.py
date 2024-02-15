58b613cc984ad6c2e5a5933250fe94b387aaffc5
BramProCoder <97104190@st.deltion.nl>
2024-02-15 16:36:12 +0100

    Opdracht 1, 1

--- opdrachten/010_variabelen/opdr_1/opdr_1.py
+++ opdrachten/010_variabelen/opdr_1/opdr_1.py
@@ -1,10 +1,8 @@
 # Opdracht 1
-# Naam student:
-# Groep:
+# Naam student: Bram Pierik
+# Groep:Itx1
 
-# Hier komt je code...
+# Hier komt je code  
 
-my_list = 
-
-fea,u,u7uyumj 
-htyhawdacvv
+my_list = [8, 12.5, "wat een weertje", 13]
+print(my_list)
