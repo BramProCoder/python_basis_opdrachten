dc10071afbb95e0505ac4d0b8ab11de3f37bb91c
BramProCoder <97104190@st.deltion.nl>
2024-03-01 10:28:54 +0100

    graden meuk

--- opdrachten/030_berekeningen/opdr_2/opdr_2.py
+++ opdrachten/030_berekeningen/opdr_2/opdr_2.py
@@ -4,7 +4,22 @@
 
 # Hier komt je code...
 
-c = ...
-f = ...
+c = -12
+f = 144
 
+# Convert Celsius to Fahrenheit
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9//5) + 32
+    print(f"{celsius} graden Celsius is gelijk aan {fahrenheit} graden Fahrenheit")
+
+# Convert Fahrenheit to Celsius
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5//9
+    print(f"{fahrenheit} graden Fahrenheit is gelijk aan {celsius} graden Celsius")
+
+# Test the functions
+celsius_to_fahrenheit(-12)
+fahrenheit_to_celsius(144)
+celsius_to_fahrenheit(62.2)
+fahrenheit_to_celsius(96)
 print()
