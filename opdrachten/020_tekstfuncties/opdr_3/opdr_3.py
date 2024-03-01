10b6e99d1ad7aec318d4ee964bb758e773e87d5f
BramProCoder <97104190@st.deltion.nl>
2024-03-01 09:25:09 +0100

    KerstBOOM

--- opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
@@ -1,5 +1,14 @@
 # Opdracht 3 tekstfuncties
 # Naam student:
 # Groep:
+def print1():
+    print("    *      " * 5)    
+    print("   ***     " * 5)
+    print("  *****    " * 5)
+    print(" *******   " * 5)
+    print(" ********  " * 5)
+    print("    ***    " * 5)
+    print("    ***    " * 5)
+    print("    ***    " * 5)
 
-# Hier komt je code...
+print1()
