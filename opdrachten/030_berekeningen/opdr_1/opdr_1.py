db7eefd9626693ab7b3a519b8184ba1df0ea63af
BramProCoder <97104190@st.deltion.nl>
2024-03-01 10:02:49 +0100

    bij elkaar optellen

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -2,4 +2,13 @@
 # Naam student:
 # Groep:
 
-# Hier komt je code...
+joe = 23
+bob = 15
+fred = 22
+
+totaal = 3
+totaal_sum = joe +  bob + fred
+
+gemiddeld = totaal_sum // totaal # De som van de le
+
+print ("de gemiddelde leeftijd", gemiddeld)
