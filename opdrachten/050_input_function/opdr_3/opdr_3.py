7b17109ed61463e49fe9581f8744ef070a912b5a
BramProCoder <97104190@st.deltion.nl>
2024-03-08 11:24:40 +0100

    5 objecten

--- opdrachten/050_input_function/opdr_3/opdr_3.py
+++ opdrachten/050_input_function/opdr_3/opdr_3.py
@@ -2,5 +2,17 @@
 # Naam student:
 # Groep:
 
-# Hier komt je code...
+# Vraag de gebruiker om een lijst van minimaal 5 objecten in te voeren
+input_string = input("Voer minimaal 5 objecten gescheiden door komma's in: ")
+
+# Maak een lijst van de ingevoerde objecten, gescheiden door komma's
+objecten = input_string.split(", ")
+
+# Sorteer de lijst op alfabetische volgorde met woorden die met "z" beginnen vooraan
+objecten.sort(key=lambda x: (not x.startswith('z'), x))
+
+# Print de gesorteerde lijst
+print(objecten)
+
+
 
