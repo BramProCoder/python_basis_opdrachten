3e82cf840eaf6cae28b146e9e9ff59fd640cdd09
BramProCoder <97104190@st.deltion.nl>
2024-03-08 11:14:35 +0100

    gasten

--- opdrachten/050_input_function/opdr_2/opdr_2.py
+++ opdrachten/050_input_function/opdr_2/opdr_2.py
@@ -4,4 +4,24 @@
 
 # Hier komt je code...
 
-gasten = ["Jij", ... ]
+# Maak een lijst met de oorspronkelijke gasten
+gasten = ["Jij", "Paul", "Kees", "Marie", "Hilda"]
+
+# Voeg jezelf toe aan de lijst
+gasten.insert(0, "Bram")
+
+# Print de lijst met gasten
+print(gasten)
+
+# Verwijder Marie uit de lijst
+gasten.remove("Marie")
+
+# Print de bijgewerkte lijst met gasten
+print(gasten)
+
+# Voeg George naast Kees toe
+kees_index = gasten.index("Kees")
+gasten.insert(kees_index + 1, "George")
+
+# Print de uiteindelijke lijst met gasten
+print(gasten)
