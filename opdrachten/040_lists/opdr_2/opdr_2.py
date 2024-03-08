73a1fcd17e2419a4bddc40b11e39f015d5506356
BramProCoder <97104190@st.deltion.nl>
2024-03-08 11:09:13 +0100

    rivieren

--- opdrachten/040_lists/opdr_2/opdr_2.py
+++ opdrachten/040_lists/opdr_2/opdr_2.py
@@ -3,13 +3,16 @@
 # Groep:
 
 
-rivier_info = {
-    "rijn": ["nederland", "duitsland", "Frankrijk"],
-    "maas": ["nederland", "belgië", "duitsland"],
-    "nijl": ["egypte", "soedan", "oeganda"]
+rivier_info = { 
+    "rijn": ["nederland", "duitsland", "Frankrijk"], 
+    "maas": ["nederland", "belgië", "duitsland"], 
+    "nijl": ["egypte", "soedan", "oeganda"] 
 }
 
 rivieren = list(rivier_info.keys())
-# rivieren is nu een list met alleen de riviernamen: ['rijn', 'maas', 'nijl']
 
-# Hier jouw code.....
+print("De rivier", rivieren[0].capitalize(), "stroomt onder andere door", rivier_info[rivieren[0]][1].capitalize())
+
+print("De rivier", rivieren[1].capitalize(), "stroomt onder andere door", rivier_info[rivieren[1]][0].capitalize())
+
+print("De rivier", rivieren[2].capitalize(), "stroomt onder andere door", rivier_info[rivieren[2]][2].capitalize())
