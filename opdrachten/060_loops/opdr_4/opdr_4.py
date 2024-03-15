f09e89ea4036b57c24c7ea19fca8022eebc4f5ca
BramProCoder <97104190@st.deltion.nl>
2024-03-15 09:51:42 +0100

    2 regels

--- opdrachten/060_loops/opdr_4/opdr_4.py
+++ opdrachten/060_loops/opdr_4/opdr_4.py
@@ -1,9 +1,34 @@
-# Opdracht 3 input functie
-# Naam student:
-# Groep:
+lijn_punten= [4*x + 7 for x in range(1,10)]
+print (lijn_punten)
 
-# Hier komt je code...
 
-# Hier start de for-loop
 
-my_list = []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Gebruik precies twee regels code om de volgende opdracht te maken.  
+#Maak een comprehensive list waarbij de y waarde voor punten op een lijn worden berekend.
+
+#De formule voor de lijn is: y = 4x + 7
+#Bereken met een range van 1 tot 10 wat de uitkomst is voor y
+
+#Output:
+#> [11, 15, 19, 23, 27, 31, 35, 39, 43]
