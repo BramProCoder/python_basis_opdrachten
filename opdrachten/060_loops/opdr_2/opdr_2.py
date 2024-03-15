4a4c6a70078ef61d468a732114f6e6165d1e5a28
BramProCoder <97104190@st.deltion.nl>
2024-03-15 09:36:12 +0100

    0,50 lijst print

--- opdrachten/060_loops/opdr_2/opdr_2.py
+++ opdrachten/060_loops/opdr_2/opdr_2.py
@@ -6,5 +6,11 @@
 
 # Hier start de for-loop
 
-my_list = []
+#lege lijst voor de getallen 
+getallen_list = []
 
+#een loop gebruiken om getallen van 10 tot 50 toe te voegen
+for pik in range(0, 60, 10):
+    getallen_list.append(pik)
+
+print(getallen_list)
