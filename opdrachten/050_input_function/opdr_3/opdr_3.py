e5e67a42e825e3d0474a24004002dde58e9819fb
BramProCoder <97104190@st.deltion.nl>
2024-03-15 09:25:38 +0100

    loop + lijst

--- opdrachten/050_input_function/opdr_3/opdr_3.py
+++ opdrachten/050_input_function/opdr_3/opdr_3.py
@@ -1,18 +1,14 @@
 # Opdracht 3 input functie
 # Naam student:
 # Groep:
+my_list =[]
 
-# Vraag de gebruiker om een lijst van minimaal 5 objecten in te voeren
-input_string = input("Voer minimaal 5 objecten gescheiden door komma's in: ")
-
-# Maak een lijst van de ingevoerde objecten, gescheiden door komma's
-objecten = input_string.split(", ")
-
-# Sorteer de lijst op alfabetische volgorde met woorden die met "z" beginnen vooraan
-objecten.sort(key=lambda x: (not x.startswith('z'), x))
+my_list.insert(0, input("Item 1 :"))
+my_list.insert(1, input("Item 2 :"))
+my_list.insert(2, input("Item 3 :"))
+my_list.insert(3, input("Item 4 :"))
+my_list.insert(4, input("Item 5 :"))
 
 # Print de gesorteerde lijst
-print(objecten)
-
-
+print(sorted(my_list, reverse=True))
 
