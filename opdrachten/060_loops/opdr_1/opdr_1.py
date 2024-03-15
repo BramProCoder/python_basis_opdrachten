2ca229038ddd86fac52a4d873761535e6f73d75a
BramProCoder <97104190@st.deltion.nl>
2024-03-15 09:24:42 +0100

    loop + lijst

--- opdrachten/060_loops/opdr_1/opdr_1.py
+++ opdrachten/060_loops/opdr_1/opdr_1.py
@@ -6,4 +6,14 @@
 
 # Hier start de for-loop....
 
-my_list = []
+my_list = [1,2,3,4,5,6,7,8,9,10]
+
+# Een lege lijst maken om de getallen op te slaan
+getallen_lijst = []
+
+# Een loop gebruiken om de getallen van 1 t/m 10 toe te voegen aan de lijst
+for i in range(1, 11):
+    getallen_lijst.append(i)
+
+# De lijst printen
+print(getallen_lijst)
