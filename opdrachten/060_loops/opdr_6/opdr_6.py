e1e6ab4d575cce60829a55ea1de961bcbfe0e728
BramProCoder <97104190@st.deltion.nl>
2024-03-15 10:28:07 +0100

    pizza

--- opdrachten/060_loops/opdr_6/opdr_6.py
+++ opdrachten/060_loops/opdr_6/opdr_6.py
@@ -1,9 +1,18 @@
-# Opdracht 3 input functie
-# Naam student:
-# Groep:
+pizza_lijst = ['margharita', 'calzone', 'verdi', 'olivio', 'quattro stagioni']
+print(pizza_lijst)
 
-# Hier komt je code...
+#sorteer
+pizza_lijst.sort()
+print(pizza_lijst)
 
-# Hier start de for-loop
+pizza_lijst.append('beste-pizza-in-de-game')
+print(pizza_lijst)
 
-my_list = []
+pizza_lijst.remove('olivio')
+print(pizza_lijst)
+
+print(pizza_lijst[:3])
+
+print([pizza_lijst[len(pizza_lijst)//2]])
+
+print(pizza_lijst[-3:])
